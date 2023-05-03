c846d21d3f866982e8b9647c559c32a6522526c3
Janice Chen <janicechen31997@hotmail.com>
2023-05-02 20:03:22 -0500

    commit

---
+++ Micv/src/components/portafolio/design/design.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+const design = () => {
+  return (
+    <div>
+      <div className="portfolio">
+  <div className="portfolio-heading">
+    <h2>My Portfolio</h2>
+    <div className="divider" />
+  </div>
+  <div className="container">
+    <div className="card">
+      <div className="card__img">
+      </div>
+      <div className="card__title">
+        <h2>Project Description</h2>
+      </div>
+      <div className="card__proyect-links">
+        <a href="#">
+          <i className="fab fa-github" />
+        </a>
+        <a href>
+          <i className="fas fa-globe-asia" />
+        </a>
+        <a href>
+          <i className="fas fa-code" />
+        </a>
+      </div>
+      <div className="button-content">
+        <button className="project-details-button download-cv">
+          <a href="#">Project Details</a>
+        </button>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card__img">
+      </div>
+      <div className="card__title">
+        <h2>Project Description</h2>
+      </div>
+      <div className="card__proyect-links">
+        <a href="#">
+          <i className="fab fa-github" />
+        </a>
+        <a href>
+          <i className="fas fa-globe-asia" />
+        </a>
+        <a href>
+          <i className="fas fa-code" />
+        </a>
+      </div>
+      <div className="button-content">
+        <button className="project-details-button download-cv">
+          <a href="#">Project Details</a>
+        </button>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card__img">
+      </div>
+      <div className="card__title">
+        <h2>Project Description</h2>
+      </div>
+      <div className="card__proyect-links">
+        <a href="#">
+          <i className="fab fa-github" />
+        </a>
+        <a href>
+          <i className="fas fa-globe-asia" />
+        </a>
+        <a href>
+          <i className="fas fa-code" />
+        </a>
+      </div>
+      <div className="button-content">
+        <button className="project-details-button download-cv">
+          <a href="#">Project Details</a>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default design
