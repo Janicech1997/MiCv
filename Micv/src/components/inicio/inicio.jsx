67ff672f7a82248f37de16c92c9fd2e010b8b085
Janice Chen <janicechen31997@hotmail.com>
2023-05-06 14:42:52 -0500

    actualizado

--- Micv/src/components/inicio/inicio.jsx
+++ Micv/src/components/inicio/inicio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from "../Layout/Header"
 import Footer from "../Layout/Footer"
 import classes from "../inicio/Inicio.module.css"
-import { BsGithub, BsTiktok, BsLinkedin, BsTwitch } from "react-icons/bs";
+import { BsGithub, BsBehance, BsLinkedin } from "react-icons/bs";
 
 
 
@@ -25,10 +25,9 @@ const inicio = () => {
   </b>
     <div className={classes.redes}>
       <span className={classes.redes}><ul className={classes.titulu}>
-         <li><a href="https://github.com/Janicech1997"><BsGithub/></a></li>
-         <li><a href="https://www.linkedin.com/in/janice-chen-31ab47152/"><BsLinkedin/></a></li>
-         <li><a href="https://www.tiktok.com/@heyjaniz"><BsTiktok/></a></li>
-         <li><a href="https://www.twitch.tv/heyjaniz"><BsTwitch/></a></li>
+         <li><a href="https://github.com/Janicech1997" target="_blank"><BsGithub/></a></li>
+         <li><a href="https://www.linkedin.com/in/janice-chen-31ab47152/" target="_blank"><BsLinkedin/></a></li>
+         <li><a href="https://www.behance.net/janicechen1997" target="_blank"><BsBehance/></a></li>
       </ul></span>
     </div>
 </div>
